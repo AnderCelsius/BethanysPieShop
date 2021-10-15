039ee32094ed0dcec50ffec36cbaee8dcaf7928f
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-15 08:43:04 +0100

    Create Models

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,10 +1,11 @@
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
+using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,6 +24,9 @@ namespace BethanysPieShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
