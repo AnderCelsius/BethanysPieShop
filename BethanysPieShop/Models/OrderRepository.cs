65bae454521131d53d999f3d51ac6811b11c62fc
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-15 22:18:28 +0100

    Add Order Repository

---
+++ BethanysPieShop/Models/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BethanysPieShop.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderRepository(AppDbContext context, ShoppingCart shoppingCart)
+        {
+            _context = context;
+            _shoppingCart = shoppingCart;
+        }
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+            _context.Orders.Add(order);
+            
+            var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+
+            foreach(var shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    OrderId = order.OrderId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                _context.OrderDetails.Add(orderDetail);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
