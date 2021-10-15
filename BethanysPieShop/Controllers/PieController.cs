30a7b8a66a73ddb57f0f7e0dfcfcc87a38e78860
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-15 22:08:29 +0100

    Order Added

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,6 +1,8 @@
 ﻿using BethanysPieShop.Models;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BethanysPieShop.Controllers
@@ -15,14 +17,29 @@ namespace BethanysPieShop.Controllers
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         } 
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            var pieListViewModel = new PieListViewModel
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
             {
-                Pies = _pieRepository.AllPies,
-                CurrentCategory = "Cheese cakes"
-            };
-            return View(pieListViewModel);
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies
+               .Where(p => p.Category.CategoryName == category)
+               .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }           
+
+            return View(new PieListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         public async Task<IActionResult> Details(int id)
