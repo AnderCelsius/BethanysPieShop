16d0d35e3d8003bdef9a9a3939ec0a287302a13b
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-15 14:54:59 +0100

    Create PieRepository and CategoryRepository Add Database Connection Seed Data to Database

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Category> AllCategories => _context.Categories;
+    }
+}
